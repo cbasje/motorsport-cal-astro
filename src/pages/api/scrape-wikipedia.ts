c0f13a0ffef6fbd0068af39345e5dd767ebb2ac9
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-16 14:38:54 +0200

    Add scrape-wikipedia.ts

---
+++ src/pages/api/scrape-wikipedia.ts
@@ -0,0 +1,19 @@
+import type { APIRoute } from "astro";
+import { main } from "../../../lib/wikipedia";
+
+export const get: APIRoute = async () => {
+    try {
+        const data = await main();
+
+        return new Response(
+            JSON.stringify({
+                success: true,
+                data,
+            })
+        );
+    } catch (error) {
+        return new Response(JSON.stringify({ success: false }), {
+            status: 500,
+        });
+    }
+};
