34531f94c7d252d29c8fbdfd2b6bce48b5d87a21
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-16 13:05:25 +0200

    Create reset-database.ts

---
+++ src/pages/api/reset-database.ts
@@ -0,0 +1,36 @@
+import type { APIRoute } from "astro";
+import prisma from "../../../lib/prisma-client";
+
+export const get: APIRoute = async ({ request }) => {
+    try {
+        const params = new URL(request.url).searchParams;
+        const sure = params.get("sure") === "true";
+
+        if (!sure)
+            return new Response(
+                JSON.stringify({
+                    success: false,
+                    reason: "Not 'sure'",
+                }),
+                {
+                    status: 400,
+                    statusText: "Not 'sure'",
+                }
+            );
+
+        await prisma.session.deleteMany();
+        await prisma.round.deleteMany();
+        await prisma.circuit.deleteMany();
+
+        return new Response(
+            JSON.stringify({
+                success: true,
+            })
+        );
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ success: false, reason: error }), {
+            status: 500,
+        });
+    }
+};
