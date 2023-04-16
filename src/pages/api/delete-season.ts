bcb88c67d1de171d13c29d85c1362e2878fd0057
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-16 12:57:35 +0200

    Create delete-season.ts

---
+++ src/pages/api/delete-season.ts
@@ -0,0 +1,62 @@
+import type { APIRoute } from "astro";
+import prisma from "../../../lib/prisma-client";
+
+export const get: APIRoute = async ({ request }) => {
+    try {
+        const params = new URL(request.url).searchParams;
+        const year = params.get("year");
+        const includeCurrent = params.get("include_current") === "true";
+
+        if (!year)
+            return new Response(
+                JSON.stringify({
+                    success: false,
+                    reason: "No 'year' param included",
+                }),
+                {
+                    status: 400,
+                    statusText: "No 'year' param included",
+                }
+            );
+
+        const sessions = await prisma.session.deleteMany({
+            where: {
+                round: {
+                    AND: {
+                        season: {
+                            contains: year,
+                            not: includeCurrent
+                                ? {
+                                      contains: String(
+                                          new Date().getFullYear()
+                                      ),
+                                  }
+                                : undefined,
+                        },
+                    },
+                },
+            },
+        });
+
+        if (!sessions.count)
+            return new Response(
+                JSON.stringify({ success: false, reason: "Nothing deleted" }),
+                {
+                    status: 500,
+                    statusText: "Nothing deleted",
+                }
+            );
+
+        return new Response(
+            JSON.stringify({
+                success: true,
+                data: { year, includeCurrent },
+            })
+        );
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify({ success: false, reason: error }), {
+            status: 500,
+        });
+    }
+};
