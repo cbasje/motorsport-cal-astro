0a3a32c45ba275d32d388e6acdc7c173da1fbcf8
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-19 14:55:57 +0200

    Remove migrations

--- prisma/migrations/20230419070233_unique_round/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,series,season]` on the table `Round` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Round_title_series_season_key" ON "Round"("title", "series", "season");
