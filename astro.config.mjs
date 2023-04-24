04417763e3de087b651e7a55d966eddbcd5da8a7
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-24 15:28:47 +0200

    Add auth-astro package + Github provider

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,26 @@ import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import auth from "auth-astro";
+import GitHub from "@auth/core/providers/github";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: netlify(),
-    integrations: [tailwind(), svelte()],
+    integrations: [
+        tailwind(),
+        svelte(),
+        auth({
+            providers: [
+                GitHub({
+                    clientId: import.meta.env.GITHUB_CLIENT_ID,
+                    clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+                }),
+            ],
+        }),
+    ],
     site: import.meta.env.PROD
-        ? "https://motorsport-cal-astro.netlify.app"
+        ? "https://motorsport.benjami.in"
         : "http://localhost:3000",
 });
