0333afc386db952daadfa973a08a9b5195194496
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2023-04-26 10:25:23 +0200

    Remove tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import netlify from "@astrojs/netlify/functions";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
     adapter: netlify(),
-    integrations: [tailwind(), svelte()],
+    integrations: [svelte()],
     site: import.meta.env.PROD
         ? "https://motorsport-cal-astro.netlify.app"
         : "http://localhost:3000",
