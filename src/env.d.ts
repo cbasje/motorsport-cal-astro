8b3f900e5414262e7bb4bdeb131d48f860c55acd
Sebastiaan Benjamins <sebastiaan@benjami.in>
2023-11-05 11:51:10 +0100

    Fix fonts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,14 +3,7 @@
 
 interface ImportMetaEnv {
     readonly DATABASE_URL: string;
-    readonly DATABASE_DEV_URL: string;
-    readonly INCLUDED_SERIES: string;
-
-    readonly F1A_API_KEY: string;
-    readonly F2_API_KEY: string;
-    readonly F3_API_KEY: string;
-
-    readonly MAP_API_KEY: string;
+    readonly PUBLIC_MAP_API_KEY: string;
 }
 
 interface ImportMeta {
